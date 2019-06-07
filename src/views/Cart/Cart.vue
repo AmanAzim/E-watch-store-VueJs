3e8e32e4e169b0cd81390c475bba9b8dd9262650
Aman <rumman.djuice1@gmail.com>
2019-06-07 23:57:56 +0200

    commit

--- src/views/Cart/Cart.vue
+++ src/views/Cart/Cart.vue
@@ -18,13 +18,14 @@
   import CartColumns from './CartColumns.vue';
   import CartList from './CartList.vue';
   import CartTotal from './CartTotal.vue';
+
   export default {
     components:{
       EmptyCart,
       Title,
       CartColumns,
       CartList,
-      CartTotal
+      CartTotal,
     }
   }
 </script>
--- src/views/Cart/CartTotal.vue
+++ src/views/Cart/CartTotal.vue
@@ -6,18 +6,36 @@
                 <h5><span class="text-title">Subtotal : </span><b>${{$store.state.cartSubtotal}}</b></h5>
                 <h5><span class="text-title">Tax : </span><b>${{$store.state.cartTax}}</b></h5>
                 <h5><span class="text-title">Cart Total : </span><b>${{$store.state.cartTotal}}</b></h5>
-
+                <PayPal :amount="$store.state.cartTotal"
+                        currency="EUR"
+                        :client="credentials"
+                        env="sandbox"></PayPal>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+  import PayPal from 'vue-paypal-checkout';
   export default {
+    components:{
+      PayPal,
+    },
+    data(){
+      return {
+        credentials: {
+          sandbox: process.env.VUE_APP_PAYPAL_ID, //'AU8laELdXXl-7xCUeRdzUN7DKMmXPqukW0cdS6CrUt5aweNtstFzEUvdX8vVdzhDP6GXToI3sq6W5sku',
+          production: '<production client id>'
+        },
+      }
+    },
     methods:{
       clearCart(){
         this.$store.dispatch('clearCart');
       }
+    },
+    created(){
+      //console.log('env',process.env.VUE_APP_PAYPAL_ID)
     }
   }
 </script>
